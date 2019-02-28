f941c65bb1fe9ad712b730831ca61c50e7899dac
Joel Thoms <jthoms@paciolan.com>
2019-02-28 10:09:28 -0800

    externalize Card styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import styled from 'styled-components'
 import DevTo from '../icons/DevTo'
 import Github from '../icons/Github'
 import Twitter from '../icons/Twitter'
 import gotoUrl from '../lib/gotoUrl'
 import ProfileImage from '../ProfileImage'
 import Button from './Button'
+import { CardStyle } from './Card.style'
 import Description from './Description'
 import Divider from './Divider'
 import Head from './Head'
 import Name from './Name'
 import Tagline from './Tagline'
 
-const Card = styled(
+const Card = CardStyle(
   ({
     className,
     name,
@@ -54,24 +54,6 @@ const Card = styled(
       <Button onClick={gotoUrl(buttonLink)}>{buttonText}</Button>
     </div>
   )
-)`
-  /* box shadow */
-  -webkit-box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.33);
-  -moz-box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.33);
-  box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.33);
-
-  /* border radius */
-  border-radius: 25px 25px 25px 25px;
-  -moz-border-radius: 25px 25px 25px 25px;
-  -webkit-border-radius: 25px 25px 25px 25px;
-  border: 0px solid #000000;
-
-  font-family: Raleway, sans-serif;
-  background-color: white;
-  color: #808080;
-  overflow: hidden;
-  width: 300px;
-  padding-bottom: 25px;
-`
+)
 
 export default Card
---
+++ src/components/Card/Card.style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const CardStyle = Component => styled(Component)`
+  /* box shadow */
+  -webkit-box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.33);
+  -moz-box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.33);
+  box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.33);
+
+  /* border radius */
+  border-radius: 25px 25px 25px 25px;
+  -moz-border-radius: 25px 25px 25px 25px;
+  -webkit-border-radius: 25px 25px 25px 25px;
+  border: 0px solid #000000;
+
+  font-family: Raleway, sans-serif;
+  background-color: white;
+  color: #808080;
+  overflow: hidden;
+  width: 300px;
+  padding-bottom: 25px;
+`
